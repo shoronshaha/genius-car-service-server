3faf068647a13b1cdb2ca4f999498e0483aeace8
shoronshaha <smaranshaha@gmail.com>
2022-04-29 17:55:35 +0600

    added steps

--- __server_deploy.js
+++ __server_deploy.js
@@ -20,6 +20,11 @@ UPDATE SERVER with new changes
 1.Make changes
 2. make sure you : git add. git commit. git push
 3. git push heroku main
-connect server with client
+-----------------------------------------
+connect server with client and deploy client
+--------------------------------------
+1. replace localhost by heroku link
+2. npm run build 
+3. firebase deploy
 
 */
